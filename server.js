c6c14b8e2f80ca3c304b570c7d002ae14a635372
asebastian <asebastian@paypal.com>
2015-08-10 13:56:47 +0530

    updated compilation error

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/', function (req, res) {
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 4000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-server.listen(server_port, server_ip_address, function(){
+app.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
